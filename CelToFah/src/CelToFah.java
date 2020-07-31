87876cf66376aaa405b31394ee80c03d4d5ef8ef
Hongki <ghdrl0727@naver.com>
2020-07-31 13:45:31 +0900

    화씨 섭씨 변환

---
+++ CelToFah/src/CelToFah.java
@@ -0,0 +1,17 @@
+
+public class CelToFah {
+
+	public static void main(String[] args) {
+		
+		double f = 36.5;
+		double c = 36.5;
+		
+		double fc = (f-32)/1.8;
+		double cf = (c*1.8)+32;
+		
+		System.out.println("È­¾¾¸¦ ¼·¾¾·Î º¯È¯:" + fc);
+		System.out.println("¼·¾¾¸¦ È­¾¾·Î º¯È¯:" + cf);
+		
+	}
+
+}
