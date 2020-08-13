b52bc2edf3f70968a6a271e8a34faaa828dc0fd5
Hongki <ghdrl0727@naver.com>
2020-08-13 17:00:44 +0900

    람다

---
+++ 람다/src/람다/CalcTest.java
@@ -0,0 +1,11 @@
+package ¶÷´Ù;
+
+public class CalcTest {
+
+	public static void main(String[] args) {
+
+		Calc calc = (num1, num2) -> (num1 + num2);
+		System.out.println(calc.add(13, 7));
+	}
+
+}
